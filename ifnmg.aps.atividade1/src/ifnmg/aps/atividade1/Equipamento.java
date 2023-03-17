ebb524790485e3deafd4576dcb01da63271fb509
Talita Souza <tatileto20@gmail.com>
2023-03-17 20:00:22 -0300

    setId e setNome
    Adicionando: setId e setNome .

--- ifnmg.aps.atividade1/src/ifnmg/aps/atividade1/Equipamento.java
+++ ifnmg.aps.atividade1/src/ifnmg/aps/atividade1/Equipamento.java
@@ -4,12 +4,17 @@ public class Equipamento {
     private int id;
     private String nome;
 
-    
     public int getId() {
         return id;
     }
     public String getNome() {
         return nome;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
     }
 
     
