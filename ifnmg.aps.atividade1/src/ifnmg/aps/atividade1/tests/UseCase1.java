5a4a3c69e81b62f618abfa844ff1b505396be907
JV-Amorim <joaovitoramorimx@gmail.com>
2023-03-19 20:49:16 -0300

    adicionando JUnit

---
+++ ifnmg.aps.atividade1/src/ifnmg/aps/atividade1/tests/UseCase1.java
@@ -0,0 +1,24 @@
+package ifnmg.aps.atividade1.tests;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class UseCase1 {
+
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+	}
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
--- ifnmg.aps.atividade1/src/module-info.java
+++
@@ -1,2 +0,0 @@
-module ifnmg.aps.atividade1 {
-}
